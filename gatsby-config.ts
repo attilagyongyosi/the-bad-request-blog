ca803fd987cc865f6798d6dc07f9f8f1ec32d777
attilagyongyosi <attilagyongyosi0@gmail.com>
2022-10-21 21:26:39 +0100

    chore: initial commit

---
+++ gatsby-config.ts
@@ -0,0 +1,10 @@
+import type { GatsbyConfig } from 'gatsby';
+
+const config: GatsbyConfig = {
+    siteMetadata: {
+        title: 'The Bad Request Blog',
+    },
+    plugins: [],
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const IndexPage = () => (
+  <h1>This is a beginning of a beautiful friendship</h1>
+);
+
+export default IndexPage;
