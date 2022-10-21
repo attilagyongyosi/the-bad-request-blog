5156053c44bea812505ac1e6e197093328d854f2
attilagyongyosi <attilagyongyosi0@gmail.com>
2022-10-21 22:19:07 +0100

    feat: integrate ESLint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const IndexPage = () => (
-  <h1>This is a beginning of a beautiful friendship</h1>
+    <h1>This is a beginning of a beautiful friendship</h1>
 );
 
 export default IndexPage;
