b5d4f9845e4871981a0644282da6a41cd055ef3c
Attila Gyöngyösi <attilagyongyosi0@gmail.com>
2022-11-11 18:41:15 +0100

    feat: designing and styling header
    Signed-off-by: Attila Gyöngyösi <attilagyongyosi0@gmail.com>

--- src/components/layout/header.component.tsx
+++ src/components/layout/header.component.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
+import * as styles from './header.module.scss';
 
 export const HeaderComponent: React.FC = () => {
-    return <header id="br-header" data-testid="br-header">The Bad Request Blog</header>;
+    return <header id={styles.header} data-testid="br-header">
+        <p id={styles.headline}>
+            <span id={styles.bad}>Bad</span> Request
+        </p>
+        <span id={styles.sub}>opinionated tech blog from attilagyongyosi</span>
+    </header>;
 };
