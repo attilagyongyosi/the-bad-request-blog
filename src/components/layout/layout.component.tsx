dbabca0470f233a95507895f00a4c1a6d6004da8
Attila Gyöngyösi <attilagyongyosi0@gmail.com>
2022-11-11 19:11:33 +0100

    fix: adding padding to main area
    Signed-off-by: Attila Gyöngyösi <attilagyongyosi0@gmail.com>

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { FooterComponent } from './footer.component';
 import { HeaderComponent } from './header.component';
+import * as styles from './layout.module.scss';
 
 export const LayoutComponent: React.FC<React.PropsWithChildren> = (props) => {
     return <>
         <HeaderComponent/>
-        <main id="br-main">
+        <main id={styles.main}>
             { props.children }
         </main>
         <FooterComponent/>
