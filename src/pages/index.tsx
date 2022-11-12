738981b8ddb3b20c1166cd1b653b2f0250db9eeb
Attila Gyöngyösi <attilagyongyosi0@gmail.com>
2022-11-12 15:33:26 +0100

    fix: removing LayoutComponent from IndexPage as it will be wrapped programatically
    Signed-off-by: Attila Gyöngyösi <attilagyongyosi0@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
 import * as React from 'react';
-import { LayoutComponent } from '../components/layout/layout.component';
 import * as styles from './index.module.scss';
 
 const IndexPage: React.FC = () => {
     return (
-        <LayoutComponent>
-            <section id={styles.page}>
-                <h1 className={'red'}>Cheers! 👋🏼</h1>
-                <p>This blog is under construction! There are commits flying and such.</p>
-                <p>It would be cool if you could check back later though.</p>
-                <p>Until that, have a cheeky look at my <a href="https://github.com/attilagyongyosi" className={'red'}>GitHub</a>!</p>
-            </section>
-        </LayoutComponent>
+        <section id={styles.page}>
+            <h1 className={'red'}>Cheers! 👋🏼</h1>
+            <p>This blog is under construction! There are commits flying and such.</p>
+            <p>It would be cool if you could check back later though.</p>
+            <p>Until that, have a cheeky look at my <a href="https://github.com/attilagyongyosi" className={'red'}>GitHub</a>!</p>
+        </section>
     );
 };
 
