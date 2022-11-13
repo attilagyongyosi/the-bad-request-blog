143c7b70c39155748249c34c096f6053704beb04
Attila Gyöngyösi <attilagyongyosi0@gmail.com>
2022-11-13 19:13:04 +0100

    feat: extracting Gatsby plugin configuration code to config folder
    Signed-off-by: Attila Gyöngyösi <attilagyongyosi0@gmail.com>

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,50 +1,7 @@
-import * as dotenv from 'dotenv';
 import type { GatsbyConfig } from 'gatsby';
 import { version } from './package.json';
-import { slugify } from './src/utils/slug';
-
-function getNotionPluginConfig() {
-    const envFileName = `.env.${process.env.NODE_ENV}`;
-    console.info(`Reading environment variables from ${envFileName}...`);
-
-    dotenv.config({ path: envFileName });
-
-    const notionDatabaseId = process.env.NOTION_DATABASE_ID;
-    const notionToken = process.env.NOTION_TOKEN;
-
-    console.info(`Notion database ID successfully read from environment: ${!!notionDatabaseId}.`);
-    console.info(`Notion integration token successfully read from environment: ${!!notionToken}.`);
-
-    return {
-        token: notionToken,
-        databaseId: notionDatabaseId,
-        propsToFrontmatter: false,
-        lowerTitleLevel: true
-    };
-}
-
-const notionPostsQuery = `
-{
-    allNotion {
-        nodes {
-            id
-            title
-            createdAt
-        }
-    }
-}
-`;
-
-function serialize({ query: { site, allNotion } }) {
-    return allNotion.nodes.map(node => {
-        return {
-            title: node.title,
-            date: node.createdAt,
-            url: `${site.siteMetadata.siteUrl}/${slugify(node.title)}`,
-            guid: node.id
-        };
-    });
-}
+import { rssFeedPluginConfig } from './config/gatsby/rss-feed-config';
+import { getNotionPluginConfig } from './config/gatsby/notion-config';
 
 const config: GatsbyConfig = {
     siteMetadata: {
@@ -61,14 +18,7 @@ const config: GatsbyConfig = {
         },
         {
             resolve: 'gatsby-plugin-feed',
-            options: {
-                feeds: [{
-                    query: notionPostsQuery,
-                    output: 'rss.xml',
-                    title: 'The Bad Request Blog Feed',
-                    serialize
-                }]
-            }
+            options: rssFeedPluginConfig
         }
     ],
 };
