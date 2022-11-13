5bc583b4cc221d30d99dcd857fe5cb45c0de05e1
Attila Gyöngyösi <attilagyongyosi0@gmail.com>
2022-11-13 16:36:10 +0100

    feat: integrating gatsby-plugin-feed to generate RSS
    Signed-off-by: Attila Gyöngyösi <attilagyongyosi0@gmail.com>

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,32 +1,73 @@
 import * as dotenv from 'dotenv';
 import type { GatsbyConfig } from 'gatsby';
 import { version } from './package.json';
+import { slugify } from './src/utils/slug';
 
-const envFileName = `.env.${process.env.NODE_ENV}`;
-console.info(`Reading environment variables from ${envFileName}...`);
+function getNotionPluginConfig() {
+    const envFileName = `.env.${process.env.NODE_ENV}`;
+    console.info(`Reading environment variables from ${envFileName}...`);
 
-dotenv.config({ path: envFileName });
+    dotenv.config({ path: envFileName });
 
-const notionDatabaseId = process.env.NOTION_DATABASE_ID;
-const notionToken = process.env.NOTION_TOKEN;
+    const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+    const notionToken = process.env.NOTION_TOKEN;
 
-console.info(`Notion database ID successfully read from environment: ${!!notionDatabaseId}.`);
-console.info(`Notion integration token successfully read from environment: ${!!notionToken}.`);
+    console.info(`Notion database ID successfully read from environment: ${!!notionDatabaseId}.`);
+    console.info(`Notion integration token successfully read from environment: ${!!notionToken}.`);
+
+    return {
+        token: notionToken,
+        databaseId: notionDatabaseId,
+        propsToFrontmatter: false,
+        lowerTitleLevel: true
+    };
+}
+
+const notionPostsQuery = `
+{
+    allNotion {
+        nodes {
+            id
+            title
+            createdAt
+        }
+    }
+}
+`;
+
+function serialize({ query: { site, allNotion } }) {
+    return allNotion.nodes.map(node => {
+        return {
+            title: node.title,
+            date: node.createdAt,
+            url: `${site.siteMetadata.siteUrl}/${slugify(node.title)}`,
+            guid: node.id
+        };
+    });
+}
 
 const config: GatsbyConfig = {
     siteMetadata: {
         title: 'The Bad Request Blog',
+        description: 'Opinionated tech blog from attilagyongyosi',
+        siteUrl: 'https://blog.attilagyongyosi.dev',
         version
     },
     plugins: [
         'gatsby-plugin-sass',
         {
             resolve: 'gatsby-source-notion-api',
+            options: getNotionPluginConfig()
+        },
+        {
+            resolve: 'gatsby-plugin-feed',
             options: {
-                token: notionToken,
-                databaseId: notionDatabaseId,
-                propsToFrontmatter: false,
-                lowerTitleLevel: true
+                feeds: [{
+                    query: notionPostsQuery,
+                    output: 'rss.xml',
+                    title: 'The Bad Request Blog Feed',
+                    serialize
+                }]
             }
         }
     ],
